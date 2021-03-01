0b6adf779fcf7a20a5de5c2c1d475c435936cc66
Gabriel Soares <gabrielsoaresii@outlook.com>
2021-03-01 13:44:58 -0300

    chore: adding HTTPS server configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const https = require('https');
 
-app.listen(port, () => {
-  console.log(`App listenning on port: ${port} | REST`)
-})
+const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV !== "development";
+
+const serverOptions = {
+  key: isProduction ? fs.readFileSync(process.env.PATH_TO_PRIVATE_KEY, 'utf8') : 0,
+  cert: isProduction ? fs.readFileSync(process.env.PATH_TO_PRIVATE_KEY, 'utf8') : 0
+};
+
+if (serverOptions.key && serverOptions.cert) {
+
+  const httpsServer = https.createServer(serverOptions, app);
+  return httpsServer.listen(port);
+} else {
+
+  app.listen(port, () => {
+    console.log(
+      `App listenning on port: ${port} | REST \nSSL Cert => ${serverOptions.cert ? true : false}`
+    )
+  })
+}
